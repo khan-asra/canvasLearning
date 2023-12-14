062b7f1f5c45adc93ba3c66dfd6734201200fc2c
khan-asra <khanasr@sheridancollege.ca>
2023-12-13 23:05:52 -0500

    curves

--- javaScript.js
+++ javaScript.js
@@ -4,23 +4,30 @@ window.addEventListener('load', function () {
     const ctx = canvas.getContext('2d');
 
 
+    const canvas2 = this.document.getElementById('canvas2');
+    const ctx2 = canvas2.getContext('2d');
+
+    const canvas3 = this.document.getElementById('canvas3');
+    const ctx3 = canvas3.getContext('2d');
+
+
     //Blue rectangle 
     ctx.fillStyle = "blue"
-    ctx.fillRect(3, 40, 20, 50);
+    ctx.fillRect(2, 20, 20, 30);
 
     //Blue rectangle 
     ctx.fillStyle = "yellow"
-    ctx.fillRect(6, 80, 20, 50);
+    ctx.fillRect(4, 40, 20, 30);
 
     // green
     ctx.fillStyle = "green"
-    ctx.fillRect(9, 50, 20, 50);
+    ctx.fillRect(6, 50, 20, 30);
 
     //draw lines
     ctx.beginPath();//reset the context state
-    ctx.moveTo(10, 70);//moveTo (x,y ) starting poinyt of line
-    ctx.lineTo(30, 70) //lineTo (x,y ) ending poinyt of line
-    ctx.strokeStyle = "black" //color of line
+    ctx.moveTo(10, 10);//moveTo (x,y ) starting poinyt of line
+    ctx.lineTo(30, 10) //lineTo (x,y ) ending poinyt of line
+    ctx.strokeStyle = "red" //color of line
     ctx.lineWidth = 1
     ctx.stroke()// draw line
     ctx.lineCap = "round"
@@ -31,16 +38,16 @@ window.addEventListener('load', function () {
     for (let i = 0; i < 5; i++) {
 
         ctx.beginPath();//reset the context state
-        ctx.moveTo(x, 30);//moveTo (x,y ) starting poinyt of line
+        ctx.moveTo(x, 10);//moveTo (x,y ) starting poinyt of line
         x = x + 50;
-        ctx.lineTo(x, 90) //lineTo (x,y ) ending poinyt of line
+        ctx.lineTo(x, 70) //lineTo (x,y ) ending poinyt of line
         ctx.strokeStyle = "pink" //color of line
         ctx.stroke()// draw line
         //---------------------------------------
         ctx.beginPath();//reset the context state
-        ctx.moveTo(x, 90);//moveTo (x,y ) starting poinyt of line
+        ctx.moveTo(x, 70);//moveTo (x,y ) starting poinyt of line
         x = x + 50
-        ctx.lineTo(x, 30) //lineTo (x,y ) ending poinyt of line
+        ctx.lineTo(x, 10) //lineTo (x,y ) ending poinyt of line
         ctx.strokeStyle = "pink" //color of line
         ctx.stroke()// draw line
 
@@ -49,26 +56,26 @@ window.addEventListener('load', function () {
     x = -5
     for (let i = 0; i < 5; i++) {
         ctx.beginPath();//reset the context state
-        ctx.moveTo(x, 30);//moveTo (x,y ) starting poinyt of line
+        ctx.moveTo(x, 10);//moveTo (x,y ) starting poinyt of line
         x = x + 50;
-        ctx.lineTo(x, 90) //lineTo (x,y ) ending poinyt of line
+        ctx.lineTo(x, 70) //lineTo (x,y ) ending poinyt of line
         ctx.strokeStyle = "purple" //color of line
-        ctx.moveTo(x, 90);//moveTo (x,y ) starting poinyt of line
+        ctx.moveTo(x, 70);//moveTo (x,y ) starting poinyt of line
         x = x + 50
-        ctx.lineTo(x, 30) //lineTo (x,y ) ending poinyt of line
+        ctx.lineTo(x, 10) //lineTo (x,y ) ending poinyt of line
         ctx.stroke()// draw line
 
     }
     x = -10
     for (let i = 0; i < 5; i++) {
         ctx.beginPath();//reset the context state
-        ctx.moveTo(x, 30);//moveTo (x,y ) starting poinyt of line
+        ctx.moveTo(x, 10);//moveTo (x,y ) starting poinyt of line
         x = x + 50;
-        ctx.lineTo(x, 90) //lineTo (x,y ) ending poinyt of line
+        ctx.lineTo(x, 70) //lineTo (x,y ) ending poinyt of line
         ctx.strokeStyle = "#601ca4" //color of line
-        ctx.moveTo(x, 90);//moveTo (x,y ) starting poinyt of line
+        ctx.moveTo(x, 70);//moveTo (x,y ) starting poinyt of line
         x = x + 50
-        ctx.lineTo(x, 30) //lineTo (x,y ) ending poinyt of line
+        ctx.lineTo(x, 10) //lineTo (x,y ) ending poinyt of line
         ctx.stroke()// draw line
 
     }
@@ -142,7 +149,7 @@ window.addEventListener('load', function () {
         ctx.lineWidth = 5
         ctx.shadowColor = "black"
         ctx.shadowOffsetX = 5
-        ctx.shadowOffsetY = 5
+        ctx.shadowOffsetY = -5
         ctx.shadowBlur = 5
         ctx.moveTo(x, y + 30)
         ctx.lineTo(x + 20, y + 30)
@@ -152,9 +159,90 @@ window.addEventListener('load', function () {
 
     }
 
+    //-----------------------------------CANVAS2---------------------------
+    /*
+    ctx2.lineCap = |butt|round|square
+    ctx2.lineJoin = bevel|round|miter
+    ctx2.shadowBlur =blur amount of shadow
+    ctx2.shadowColor color of shadow
+    ctx2.shadowOffsetX = horizontal distance of the shadow from the shape
+    ctx2.shadowOffsetY =  vertical distance of the shadow from the shape
+    */
 
 
 
+    ctx2.beginPath()
+    ctx2.strokeStyle = "red"
+    ctx2.lineWidth = 10
+    ctx2.moveTo(20, 20)
+    ctx2.lineTo(60, 20)
+    ctx2.lineTo(20, 80)
+    ctx2.lineTo(60, 80)
+    ctx2.lineCap = "butt"
+    ctx2.lineJoin = "bevel"
+    ctx2.shadowBlur = 5
+    ctx2.shadowColor = "black"
+    ctx2.shadowOffsetX =10
+    ctx2.shadowOffsetY =10
+    ctx2.stroke()
+
+
+    ctx2.beginPath()
+    ctx2.strokeStyle = "blue"
+    ctx2.lineWidth = 10
+    ctx2.moveTo(120, 20)
+    ctx2.lineTo(80, 20)
+    ctx2.lineTo(120, 80)
+    ctx2.lineTo(80, 80)
+    ctx2.lineCap = "round"
+    ctx2.lineJoin = "round"
+    ctx2.shadowBlur = 5
+    ctx2.shadowColor = "black"
+    ctx2.shadowOffsetX =10
+    ctx2.shadowOffsetY =-10
+    ctx2.stroke()
+
+
+    ctx2.beginPath()
+    ctx2.strokeStyle = "red"
+    ctx2.lineWidth = 10
+    ctx2.moveTo(140, 20)
+    ctx2.lineTo(180, 20)
+    ctx2.lineTo(140, 80)
+    ctx2.lineTo(180, 80)
+    ctx2.lineCap = "square"
+    ctx2.lineJoin = "miter"
+    ctx2.shadowBlur = 5
+    ctx2.shadowColor = "black"
+    ctx2.shadowOffsetX =10
+    ctx2.shadowOffsetY =10
+    ctx2.stroke()
+
+//circle
+
+var radius = Math.PI/180
+  ctx3.beginPath()
+  ctx3.strokeStyle="purple"
+  ctx3.lineWidth= 2
+  //(x,y, starting point,endingpoint)
+  ctx3.arc(80,80,50,0 *radius,180*radius,false)
+  ctx3.stroke()
+
+
+  ctx3.beginPath()
+  ctx3.strokeStyle="purple"
+  ctx3.lineWidth= 2
+  //(x,y, starting point,endingpoint)
+  ctx3.arc(110,90,40,50 *radius,180*radius,true)
+  ctx3.stroke()
+
+//quadratic 
+  ctx3.beginPath()
+  ctx3.strokeStyle="orange"
+  ctx3.lineWidth= 2
+ctx3.moveTo(70,190)
+ctx3.quadraticCurveTo(100,30,200,150)
+ctx3.stroke()
 
 
 
